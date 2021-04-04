5476d468943a3fe57422c2376d4c62937f44bd4c
Melisa D. Bellico <melisabellico@gmail.com>
2021-04-04 18:47:48 -0300

    login.js changes

--- js/login.js
+++ js/login.js
@@ -3,9 +3,11 @@ var test = 'Validation results:';
 
 var loginForm = function () {
     if (document.querySelector('#formContainer')){
-        validationTests.append(test + "" + 'Existing Form');   
+        validationTests.style.display = 'flex';
+        validationTests.append(test + "" + 'Existing Form');
     }
     else {
+        validationTests.style.display = 'flex';
         validationTests.append(test + "" + 'Existing Form');
     }
 }
@@ -16,9 +18,11 @@ var amountOfFields = function() {
     var fieldsQuantity = fields.length;
     if (fieldsQuantity = 2){
         validationTests.append(test + "" + 'fields quantity ok');
+        validationTests.style.display = 'flex';
     }
     else {
         validationTests.append(test + "" + 'wrong quantity of fields');
+        validationTests.style.display = 'flex';
     }
 }
 
@@ -26,9 +30,11 @@ amountOfFields()
 var requiredFields = function(){
     var inputs = document.querySelectorAll('input').requiered;
     if (inputs = true){
+        validationTests.style.display = 'flex';
         validationTests.append(test + "" + 'Required Fields');
     }
     else {
+        validationTests.style.display = 'flex';
         validationTests.append(test + "" + 'Required attributes missing');
     }
 }
@@ -38,9 +44,11 @@ var labels = function(){
     var inputs =document.querySelectorAll('input').name;
     var labels = document.querySelectorAll('label').for;
     if (inputs === labels){
+        validationTests.style.display = 'flex';
         validationTests.append(test + "" + 'Inputs with associated lables');
     }
     else {
+        validationTests.style.display = 'flex';
         validationTests.append(test + "" + 'missing labels');
     }
 }
@@ -50,9 +58,11 @@ var validAnch = function (){
     var anchor = document.getElementsByClassName('submit').href;
     var validAnchor = "register.html";
     if (anchor = validAnch) {
+        validationTests.style.display = 'flex';
         validationTests.append(test + "" + 'valid anchor');
     }
     else {
+        validationTests.style.display = 'flex';
         validationTests.append(test + "" + 'wrong anchor');
     }
 }
@@ -62,9 +72,11 @@ var btnsVerification = function (){
     var submit = document.querySelector('.submit').nodeValue;
     var submitValue = 'login';
     if (submit = submitValue) {
+        validationTests.style.display = 'flex';
         validationTests.append(test + "" + 'Right Button');
     }
     else {
+        validationTests.style.display = 'flex';
         validationTests.append(test + "" + 'Wrong Button');
     }
 }
@@ -74,6 +86,7 @@ btnsVerification ();
 var lastVerif = function () {
     if (( loginForm = true) && (amountOfFields = true)) && (requiredFields = true) &&
     (validAnch = true) && (btnsVerification = true)) {
+        validationTests.style.display = 'flex';
         validationTests.textContent = test + "" + 'test validations successful';
     }
 }
