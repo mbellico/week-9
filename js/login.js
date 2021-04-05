fa3f93efae3bbb4d2fbcdd923bfd7531c6ee521f
Melisa D. Bellico <melisabellico@gmail.com>
2021-04-04 21:10:36 -0300

    Register.js added

--- js/login.js
+++ js/login.js
@@ -1,89 +1,92 @@
-
 var validationTests = document.getElementsByClassName('validationFuntions');
 var test = 'Validation results:';
-
 var loginForm = function () {
     if (document.querySelector('#formContainer')){
+        console.log(test + 'Existing forms');
         validationTests.display = 'flex';
-        validationTests.innerHTML = 'test' + "" + 'Existing Form';
+        validationTests.innerHTML = 'test' + "" + 'Wrong ButtonExisting form';
     }
     else {
+        console.log(test + 'non-existing form');
         validationTests.display = 'flex';
-        validationTests.innerHTML = 'test' + "" + 'Existing Form';
+        validationTests.innerHTML = 'test' + "" + 'non-existing form';
     }
 }
-
 loginForm ();
 var amountOfFields = function() {
     var fields = document.querySelectorAll('input');
     var fieldsQuantity = fields.length;
     if (fieldsQuantity = 2){
+        console.log(test + 'The amount of fields is correct');
         validationTests.display = 'flex';
-        validationTests.innerHTML = 'test' + "" + 'fields quantity ok';
+        validationTests.innerHTML = 'test' + "" + 'The amount of fields is correct';
     }
     else {
+        console.log(test + 'wrong quantity of fields');
         validationTests.display = 'flex';
-        validationTests.innerHTML ='test' + "" + 'wrong quantity of fields';
+        validationTests.innerHTML = 'test' + "" + 'Wrong quantity of fields';
     }
 }
-
 amountOfFields()
 var requiredFields = function(){
     var inputs = document.querySelectorAll('input').requiered;
     if (inputs = true){
+        console.log(test + 'all required fields were found')
         validationTests.display = 'flex';
-        validationTests.innerHTML = 'test' + "" + 'Required Fields';
+        validationTests.innerHTML = 'test' + "" + 'all required fields were found';
     }
     else {
+        console.log(test + 'Required attributes missing');
         validationTests.display = 'flex';
         validationTests.innerHTML = 'test' + "" + 'Required attributes missing';
     }
 }
 requiredFields();
-
 var labels = function(){
     var inputs =document.querySelectorAll('input').name;
     var labels = document.querySelectorAll('label').for;
     if (inputs === labels){
+        console.log(test + 'Inputs with associated labels');
         validationTests.display = 'flex';
-        validationTests.innerHTML = 'test' + "" + 'Inputs with associated lables';
+        validationTests.innerHTML = 'test' + "" + 'Inputs with associated labels';
     }
     else {
+        console.log(test + 'missing labels');
         validationTests.display = 'flex';
-        validationTests.innerHTML = 'test' + "" + 'missing labels';
+        validationTests.innerHTML = 'test' + "" + 'Missing Labels';
     }
 }
 labels ();
-
 var validAnch = function (){
     var anchor = document.getElementsByClassName('submit').href;
     var validAnchor = "register.html";
-    if (anchor = validAnch) {
+    if (anchor = validAnchor) {
+        console.log(test + 'valid anchor');
         validationTests.display = 'flex';
-        validationTests.innerHTML = 'test' + "" + 'valid anchor';
+        validationTests.innerHTML = 'test' + "" + 'Valid anchor';
     }
     else {
+        console.log(test + 'wrong anchor');
         validationTests.display = 'flex';
-        validationTests.innerHTML = 'test' + "" + 'wrong anchor';
+        validationTests.innerHTML = 'test' + "" + 'Wrong anchor';
     }
 }
 validAnch ();
-
 var btnsVerification = function (){
     var submit = document.querySelector('.submit').nodeValue;
     var submitValue = 'login';
     if (submit = submitValue) {
+        console.log(test + 'Right Button');
         validationTests.display = 'flex';
         validationTests.innerHTML = 'test' + "" + 'Right Button';
     }
     else {
+        console.log(test + 'wrong button');
         validationTests.display = 'flex';
         validationTests.innerHTML = 'test' + "" + 'Wrong Button';
     }
 }
-
 btnsVerification ();
-
 var lastVerif = function () {
     if (( loginForm = true) && (amountOfFields = true) && (requiredFields = true) &&
     (validAnch = true) && (btnsVerification = true)) {
@@ -92,4 +95,3 @@ var lastVerif = function () {
     }
 }
 lastVerif();
-
---
+++ js/register.js
@@ -0,0 +1,98 @@
+var validationTests = document.getElementsByClassName('validationFuntions');
+var test = 'Validation results:';
+var registerForm = function () {
+    if (document.querySelector('#formContainer')){
+        console.log(test + 'Existing forms');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'Wrong ButtonExisting form';
+    }
+    else {
+        console.log(test + 'non-existing form');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'non-existing form';
+    }
+}
+registerForm ();
+var amountOfFields = function() {
+    var fields = document.querySelectorAll('input');
+    var fieldsQuantity = fields.length;
+    if (fieldsQuantity = 4){
+        console.log(test + 'The amount of fields is correct');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'The amount of fields is correct';
+    }
+    else {
+        console.log(test + 'wrong quantity of fields');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'Wrong quantity of fields';
+    }
+}
+amountOfFields()
+var requiredFields = function(){
+    var inputs = document.querySelectorAll('input').requiered;
+    if (inputs = true){
+        console.log(test + 'all required fields were found')
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'all required fields were found';
+    }
+    else {
+        console.log(test + 'Required attributes missing');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'Required attributes missing';
+    }
+}
+requiredFields();
+var labels = function(){
+    var inputs =document.querySelectorAll('input').name;
+    var labels = document.querySelectorAll('label').for;
+    if (inputs === labels){
+        console.log(test + 'Inputs with associated labels');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'Inputs with associated labels';
+    }
+    else {
+        console.log(test + 'missing labels');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'Missing Labels';
+    }
+}
+labels ();
+var validAnch = function (){
+    var anchor = document.getElementsByClassName('toLogin').href;
+    var validAnchor = "login.html";
+    if (anchor = validAnchor) {
+        console.log(test + 'valid anchor');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'Valid anchor';
+    }
+    else {
+        console.log(test + 'wrong anchor');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'Wrong anchor';
+    }
+}
+validAnch ();
+var btnsVerification = function (){
+    var submit = document.querySelector('.submit').nodeValue;
+    var resetKey = 'reset';
+    var signInKey = 'Sign in';
+    if ((submit = resetKey) && (submit = signInKey)){
+        console.log(test + 'Right Button');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'Right Button';
+    }
+    else {
+        console.log(test + 'wrong button');
+        validationTests.display = 'flex';
+        validationTests.innerHTML = 'test' + "" + 'Wrong Button';
+    }
+}
+btnsVerification ();
+var lastVerif = function () {
+    if (( registerForm = true) && (amountOfFields = true) && (requiredFields = true) &&
+    (validAnch = true) && (btnsVerification = true)) {
+        validationTests.display = 'flex';
+        validationTests.textContent = 'test' + "" + 'test validations successful';
+    }
+}
+lastVerif();
