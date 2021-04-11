334d8706e787d1153dd88c0bf7e03408a016ca4c
Melisa D. Bellico <melisabellico@gmail.com>
2021-04-11 00:04:29 -0300

    login-validations.js updated

--- week-10/js/login-validations.js
+++ week-10/js/login-validations.js
@@ -1,4 +1,4 @@
-var registerForm = document.getElementById ('formContainer');
+var formLogin = document.getElementById ('formLogin');
 var inputs = document.querySelectorAll('.input');
 var email = document.querySelector('#email');
 var emailAlert = document.getElementsByClassName('emailAlert');
@@ -6,8 +6,8 @@ var password = document.querySelector('#password');
 var passwordAlert = document.getElementsByClassName('passwordAlert');
 
 var regExp = {
-    email: /^[a-zA-Z0-9._-]@[a-zA-Z0-9._-]\.[a-zA-Z]$/,
-    password: /(^[a-zA-Z0-9]{8,})+$/,
+    email: /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9._+-]+\.[a-zA-Z]+$/;
+    password: /(^[a-zA-Z0-9]{8,})$/;
 }
 
 var fields = {
@@ -17,33 +17,59 @@ var fields = {
 
 var formValidation = function(e) {
     switch (e.target.name) {
-        case "email":
-            if (regExp.eMail.test(e.target.value)) {
-            email.style.border = '3px. solid green';
+        case "eMail":
+            if (regExp.email.test(e.target.value)) {
+            email.style.border = '3px solid green';
             fields [email] = true;
             } else {
             emailAlert.style.display= 'block';
             fields [email] = false;
             }
         break;
-    }
-        case "password":
-            if (regExp.pass.test(e.target.value)) {
-            password.style.border = '3px. solid green';
+        case "passw":
+            if (regExp.password.test(e.target.value)) {
+            password.style.border = '3px solid green';
             fields [password] = true;
             } else {
-            passwordAlert.style.display= 'block';S
+            passwordAlert.style.display= 'block';
             fields [password] = false;
             }
         break;
     }
 }
 
-inputs.forEach(function (inputs) {
-addEventListener('blur', validateForm);
-input.addEventListener('focus', focusForm);
-)};
-
-formValidation.addEventListener('submit', function(e) {
-    e.preventDefault ();
+var focusForm = function (e) {
+    switch (e.target.name) {
+        case 'eMail':
+            email.style.color = 'black';
+        break;
+        case 'passw':
+            password.style.color = "black";
+        break;
+    }
 }
+
+
+inputs.forEach(function (input) {
+    input.addEventListener('blur', formValidation);
+    input.addEventListener('focus', focusForm);
+});
+
+formLogin.addEventListener('submit', function(e) {
+    e.preventDefault ();
+})
+
+var button = document.getElementsByClassName('submit');
+button.addEventListener('click', validations)
+
+var validations = document.getElementsByClassName('validationFuntions');
+if (fields ['email'] && fields ['password']) {
+    validations.style.display = 'block';
+    validations.textContent = 'Your login data is:' + "" + email.value + pass+value;
+} else {
+    validations.style.display = 'block';
+    validations.textContent = 'Email or Password values are wrong. Please try again'
+})
+
+
+
