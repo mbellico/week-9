12d9fcc784dcc3551773727bda8536c9d2f18999
Melisa D. Bellico <melisabellico@gmail.com>
2021-04-11 17:25:50 -0300

    JS code last updates - GET method

--- week-10/js/login-validations.js
+++ week-10/js/login-validations.js
@@ -4,21 +4,18 @@ var email = document.querySelector('#email');
 var emailAlert = document.getElementsByClassName('emailAlert');
 var password = document.querySelector('#password');
 var passwordAlert = document.getElementsByClassName('passwordAlert');
-
 var regExp = {
-    email: /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9._+-]+\.[a-zA-Z]+$/;
-    password: /(^[a-zA-Z0-9]{8,})$/;
+    eMail: /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9._+-]+\.[a-zA-Z]+$/;
+    passWord: /(^[a-zA-Z0-9]{8,})$/;
 }
-
 var fields = {
     email: false;
     password: false;
 }
-
 var formValidation = function(e) {
     switch (e.target.name) {
-        case "eMail":
-            if (regExp.email.test(e.target.value)) {
+        case "eM":
+            if (regExp.eMail.test(e.target.value)) {
             email.style.border = '3px solid green';
             fields [email] = true;
             } else {
@@ -26,50 +23,49 @@ var formValidation = function(e) {
             fields [email] = false;
             }
         break;
-        case "passw":
-            if (regExp.password.test(e.target.value)) {
+        case "pW":
+            if (regExp.passWord.test(e.target.value)) {
             password.style.border = '3px solid green';
             fields [password] = true;
             } else {
             passwordAlert.style.display= 'block';
             fields [password] = false;
             }
-        break;
     }
 }
-
 var focusForm = function (e) {
     switch (e.target.name) {
-        case 'eMail':
+        case 'eM':
             email.style.color = 'black';
         break;
-        case 'passw':
+        case 'pW':
             password.style.color = "black";
-        break;
     }
 }
-
-
 inputs.forEach(function (input) {
     input.addEventListener('blur', formValidation);
     input.addEventListener('focus', focusForm);
 });
-
 formLogin.addEventListener('submit', function(e) {
     e.preventDefault ();
-})
-
 var button = document.getElementsByClassName('submit');
-button.addEventListener('click', validations)
-
+button.addEventListener('click', validations);
 var validations = document.getElementsByClassName('validationFuntions');
 if (fields ['email'] && fields ['password']) {
     validations.style.display = 'block';
-    validations.textContent = 'Your login data is:' + "" + email.value + pass+value;
+    validations.innerHTML = 'Your login data is:' + "" + email.value + pass+value;
 } else {
     validations.style.display = 'block';
-    validations.textContent = 'Email or Password values are wrong. Please try again'
+    validations.innerHTML = 'Email or Password values are wrong. Please try again'
 })
-
-
-
+async function getEmail(){
+    fetch('https://jsonplaceholder.typicode.com/users?email=${email.value}')
+    .then(function(response){
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch()
+};
+button.onclick = function() {
+    getEmail();
+};
--- week-10/js/register-validations.js
+++ week-10/js/register-validations.js
@@ -1,32 +1,29 @@
-var registerForm = document.getElementById ('formContainer');
+var formRegister = document.getElementById ('formRegister');
 var inputs = document.querySelectorAll('.input');
-var fName = document.querySelector ('#fullName');
+var fullName = document.querySelector ('#fullName');
 var fullNameAlert = document.getElementsByClassName('fullNameAlert');
 var email = document.querySelector('#email');
 var emailAlert = document.getElementsByClassName('emailAlert');
 var password = document.querySelector('#password');
 var passwordAlert = document.getElementsByClassName('passwordAlert');
-var rPassword = document.querySelector('#repeatPassword');
-var rPasswordAlert = document.getElementsByClassName('repeatPassAlert');
-
+var repeatPassword = document.querySelector('#repeatPassword');
+var repeatPasswordAlert = document.getElementsByClassName('repeatPassAlert');
 var regExp = {
-    fullName: /^[a-zA-ZáéíóúÑñ\s]+$/,
-    eMail: /^[a-zA-Z0-9._-]@[a-zA-Z0-9._-]\.[a-zA-Z]$/,
-    pass: /(^[a-zA-Z0-9]{8,})+$/,
+    fName: /^[a-zA-ZáéíóúÑñ]+(?:\s[a-zA-ZáéíóúÑñ]+)+$/;
+    eMail: /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9._+-]+\.[a-zA-Z]+$/;
+    passWord: /(^[a-zA-Z0-9]{8,})$/;
 }
-
 var fields = {
     fullName: false;
     email: false;
     password: false;
     rPassword: false;
 }
-
 var formValidation = function(e) {
     switch (e.target.name) {
-        case "fullName":
-            if (regExp.fullName.test(e.target.value)) {
-            fName.style.color = 'green';
+        case "nameAndLastName":
+            if (regExp.fName.test(e.target.value)) {
+            fullName.style.color = 'green';
             fields [fullName] = true;
             } else {
             fullNameAlert.style.display= 'block';
@@ -43,8 +40,8 @@ var formValidation = function(e) {
             }
         break;
     }
-        case "password":
-            if (regExp.pass.test(e.target.value)) {
+        case "passwordVerif":
+            if (regExp.passWord.test(e.target.value)) {
             password.style.color = 'green';
             fields [password] = true;
             } else {
@@ -55,20 +52,56 @@ var formValidation = function(e) {
     }
         case "rPassword":
             if (e.target.value == password.value) {
-            rPassword.style.color = 'green';
+            repeatPassword.style.color = 'green';
             fields [rPassword] = true;
             } else {
-            rPasswordAlert.style.display= 'block';
-            fields [rPsassword] = false;
-        }
+            repeatPasswordAlert.style.display= 'block';
+            fields [rPassword] = false;
+    }        
+}
+
+var focusForm = function (e) {
+    switch (e.target.name) {
+        case "nameAndLastName":
+            fullName.style.color = 'black';
         break;
+        case "email":
+            email.style.color = 'black';
+        break;
+        case "passwordVerif":
+            password.style.color = 'black';
+        break;
+        case "rPassword":
+            repeatPassword.style.color = 'black';
+    }
 }
-
 inputs.forEach(function (input) {
-addEventListener('blur', validateForm);
+input.addEventListener('blur', formValidation);
 input.addEventListener('focus', focusForm);
-)};
+});
 
-form.addEventListener('submit', function(e) {
+formRegister.addEventListener('submit', function(e) {
     e.preventDefault ();
-}
+    var button = document.getElementById('signIn');
+    button.addEventListener('click', validations);
+    var validations = document.getElementsByClassName('validationFuntions');
+    if (fields ['fullname'] && fields ['email'] && fields ['password'] && fields [repeatPassword]) {
+    validations.style.display = 'block';
+    validations.innerHTML = 'Registration process successful. Your users data is:' + "" 
+    + fullName.value + email.value + password+value;
+    } else {
+    validations.style.display = 'block';
+    validations.innerHTML = 'Registration process failed. Please check your data and try again'
+    }
+})
+async function getEmail(){
+    fetch('https://jsonplaceholder.typicode.com/users?email=${email.value}')
+    .then(function(response){
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch()
+};
+button.onclick = function() {
+    getEmail();
+};
